632ad629f2acdc1cc4788547563e3dcd6cb206f6
deyrajib <dey.rajib233@gmail.com>
2022-04-12 18:09:59 -0400

    Hw

---
+++ src/hwgit/GitHw.java
@@ -0,0 +1,62 @@
+package hwgit;
+
+import ismatLove.Love;
+
+public class GitHw {
+
+	/**
+	 * bla bla 
+	 *
+	 */
+	
+	
+//		
+//	
+//	  System.out.println("My name is ");
+//	
+//	System.out.println("My age is");
+//	
+	
+	
+		
+	//	int firstName;
+//		double Double;  // primitive data vs non-primitive data
+//		float Float;    // primitive data = fixed
+//		byte Byte;      // non-primitive = not fixed or can hold value more than 1
+//		short Short;    // variable = value holder
+//		boolean Boolean;
+//		char Character;
+//		g lonLong;
+//		String name;
+//	
+//	int iD = 401;
+//	double cellNumber = 12345678;
+//	
+//	System.out.println("My id is" +" "+ iD);	// (+)is a combine a variable with a String
+//		
+//	double newId = (double)iD;
+//		
+//	System.out.println(newId);
+//	
+	
+
+public	double price = 200.10;
+static	String name = ("Raj")	;
+	int age = 37;
+	
+	public static void main(String [] args) {
+		
+         int age=24;
+		
+		System.out.println(Love.name);
+		Love priceAge =new Love();
+		
+//		System.out.println(priceAge.price);
+//		
+//		System.out.println(priceAge.age);
+		
+		
+	}
+}
+
+}
---
+++ src/hwgit/Hw.java
@@ -0,0 +1,51 @@
+package hwgit;
+
+import ismatLove.Baby;
+import ismatLove.Baby.baba;
+
+public class Hw {
+	int x = 1214;
+	double z =452.22;
+	static String  name = ("Maanav");
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		baba bA =new baba();
+	System.out.println(baba.name);
+	
+	System.out.println("iD" + " "+ bA.x);
+	}
+	
+	
+	
+	public static class baba {
+	
+		
+		
+		
+		int x = 1214;
+		double z =452.22;
+		static String  name = ("Aunonto");
+		
+		
+	public static void main(String[] args) {
+
+	Baby bY =new Baby();
+	
+	System.out.println(bY.x +" "+bY.z);
+		
+		System.out.println(Baby.name);
+		
+	}
+	
+	
+	
+	}
+	
+}
+
+}
